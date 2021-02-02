cc06148ae76513e001f527fcb659cd4b56d519d3
Tomas Nosek <tomasn@kentico.com>
2021-02-02 16:21:15 +0100

    Add initial version

---
+++ WatsonTTS_UI/WatsonTTS_UI/Home.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WatsonTTS_UI
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.authkey = new System.Windows.Forms.TextBox();
+            this.authkey_label = new System.Windows.Forms.Label();
+            this.inputfile_label = new System.Windows.Forms.Label();
+            this.outputfile_label = new System.Windows.Forms.Label();
+            this.inputfile = new System.Windows.Forms.TextBox();
+            this.outputfile = new System.Windows.Forms.TextBox();
+            this.savepresets = new System.Windows.Forms.Button();
+            this.launchtts = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.RichTextBox();
+            this.watsonurl = new System.Windows.Forms.TextBox();
+            this.watsonurl_label = new System.Windows.Forms.Label();
+            this.watsonconfig_label = new System.Windows.Forms.LinkLabel();
+            this.filesconfig = new System.Windows.Forms.Label();
+            this.inputfile_tooltip = new System.Windows.Forms.Label();
+            this.outputfile_tooltip = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // authkey
+            // 
+            this.authkey.Location = new System.Drawing.Point(158, 42);
+            this.authkey.Name = "authkey";
+            this.authkey.Size = new System.Drawing.Size(432, 22);
+            this.authkey.TabIndex = 0;
+            // 
+            // authkey_label
+            // 
+            this.authkey_label.AutoSize = true;
+            this.authkey_label.Location = new System.Drawing.Point(12, 47);
+            this.authkey_label.Name = "authkey_label";
+            this.authkey_label.Size = new System.Drawing.Size(59, 17);
+            this.authkey_label.TabIndex = 1;
+            this.authkey_label.Text = "API key:";
+            // 
+            // inputfile_label
+            // 
+            this.inputfile_label.AutoSize = true;
+            this.inputfile_label.Location = new System.Drawing.Point(12, 166);
+            this.inputfile_label.Name = "inputfile_label";
+            this.inputfile_label.Size = new System.Drawing.Size(75, 17);
+            this.inputfile_label.TabIndex = 2;
+            this.inputfile_label.Text = "Input path:";
+            // 
+            // outputfile_label
+            // 
+            this.outputfile_label.AutoSize = true;
+            this.outputfile_label.Location = new System.Drawing.Point(12, 221);
+            this.outputfile_label.Name = "outputfile_label";
+            this.outputfile_label.Size = new System.Drawing.Size(112, 17);
+            this.outputfile_label.TabIndex = 3;
+            this.outputfile_label.Text = "Output filename:";
+            // 
+            // inputfile
+            // 
+            this.inputfile.Location = new System.Drawing.Point(158, 166);
+            this.inputfile.Name = "inputfile";
+            this.inputfile.Size = new System.Drawing.Size(432, 22);
+            this.inputfile.TabIndex = 4;
+            // 
+            // outputfile
+            // 
+            this.outputfile.Location = new System.Drawing.Point(158, 221);
+            this.outputfile.Name = "outputfile";
+            this.outputfile.Size = new System.Drawing.Size(432, 22);
+            this.outputfile.TabIndex = 5;
+            // 
+            // savepresets
+            // 
+            this.savepresets.Location = new System.Drawing.Point(344, 282);
+            this.savepresets.Name = "savepresets";
+            this.savepresets.Size = new System.Drawing.Size(117, 36);
+            this.savepresets.TabIndex = 6;
+            this.savepresets.Text = "Save presets";
+            this.savepresets.UseVisualStyleBackColor = true;
+            this.savepresets.Click += new System.EventHandler(this.savepresets_Click);
+            // 
+            // launchtts
+            // 
+            this.launchtts.Location = new System.Drawing.Point(473, 282);
+            this.launchtts.Name = "launchtts";
+            this.launchtts.Size = new System.Drawing.Size(117, 36);
+            this.launchtts.TabIndex = 7;
+            this.launchtts.Text = "Launch";
+            this.launchtts.UseVisualStyleBackColor = true;
+            this.launchtts.Click += new System.EventHandler(this.launchtts_Click);
+            // 
+            // log
+            // 
+            this.log.Enabled = false;
+            this.log.Location = new System.Drawing.Point(15, 338);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(575, 246);
+            this.log.TabIndex = 8;
+            this.log.Text = "";
+            // 
+            // watsonurl
+            // 
+            this.watsonurl.Location = new System.Drawing.Point(158, 83);
+            this.watsonurl.Name = "watsonurl";
+            this.watsonurl.Size = new System.Drawing.Size(432, 22);
+            this.watsonurl.TabIndex = 10;
+            // 
+            // watsonurl_label
+            // 
+            this.watsonurl_label.AutoSize = true;
+            this.watsonurl_label.Location = new System.Drawing.Point(12, 83);
+            this.watsonurl_label.Name = "watsonurl_label";
+            this.watsonurl_label.Size = new System.Drawing.Size(92, 17);
+            this.watsonurl_label.TabIndex = 9;
+            this.watsonurl_label.Text = "Watson URL:";
+            // 
+            // watsonconfig_label
+            // 
+            this.watsonconfig_label.AutoSize = true;
+            this.watsonconfig_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.watsonconfig_label.Location = new System.Drawing.Point(12, 9);
+            this.watsonconfig_label.Name = "watsonconfig_label";
+            this.watsonconfig_label.Size = new System.Drawing.Size(162, 17);
+            this.watsonconfig_label.TabIndex = 11;
+            this.watsonconfig_label.TabStop = true;
+            this.watsonconfig_label.Text = "Watson configuration";
+            this.watsonconfig_label.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.watsonconfig_label_LinkClicked);
+            // 
+            // filesconfig
+            // 
+            this.filesconfig.AutoSize = true;
+            this.filesconfig.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filesconfig.Location = new System.Drawing.Point(12, 138);
+            this.filesconfig.Name = "filesconfig";
+            this.filesconfig.Size = new System.Drawing.Size(142, 17);
+            this.filesconfig.TabIndex = 12;
+            this.filesconfig.Text = "Files configuration";
+            // 
+            // inputfile_tooltip
+            // 
+            this.inputfile_tooltip.AutoSize = true;
+            this.inputfile_tooltip.Location = new System.Drawing.Point(158, 191);
+            this.inputfile_tooltip.Name = "inputfile_tooltip";
+            this.inputfile_tooltip.Size = new System.Drawing.Size(410, 17);
+            this.inputfile_tooltip.TabIndex = 13;
+            this.inputfile_tooltip.Text = "Full or relative path. Speak, pitch, and rate added automatically.";
+            // 
+            // outputfile_tooltip
+            // 
+            this.outputfile_tooltip.AutoSize = true;
+            this.outputfile_tooltip.Location = new System.Drawing.Point(158, 246);
+            this.outputfile_tooltip.Name = "outputfile_tooltip";
+            this.outputfile_tooltip.Size = new System.Drawing.Size(195, 17);
+            this.outputfile_tooltip.TabIndex = 14;
+            this.outputfile_tooltip.Text = "File name only without format.";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 598);
+            this.Controls.Add(this.outputfile_tooltip);
+            this.Controls.Add(this.inputfile_tooltip);
+            this.Controls.Add(this.filesconfig);
+            this.Controls.Add(this.watsonconfig_label);
+            this.Controls.Add(this.watsonurl);
+            this.Controls.Add(this.watsonurl_label);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.launchtts);
+            this.Controls.Add(this.savepresets);
+            this.Controls.Add(this.outputfile);
+            this.Controls.Add(this.inputfile);
+            this.Controls.Add(this.outputfile_label);
+            this.Controls.Add(this.inputfile_label);
+            this.Controls.Add(this.authkey_label);
+            this.Controls.Add(this.authkey);
+            this.Name = "Home";
+            this.Text = "Watson TTS UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox authkey;
+        private System.Windows.Forms.Label authkey_label;
+        private System.Windows.Forms.Label inputfile_label;
+        private System.Windows.Forms.Label outputfile_label;
+        private System.Windows.Forms.TextBox inputfile;
+        private System.Windows.Forms.TextBox outputfile;
+        private System.Windows.Forms.Button savepresets;
+        private System.Windows.Forms.Button launchtts;
+        private System.Windows.Forms.RichTextBox log;
+        private System.Windows.Forms.TextBox watsonurl;
+        private System.Windows.Forms.Label watsonurl_label;
+        private System.Windows.Forms.LinkLabel watsonconfig_label;
+        private System.Windows.Forms.Label filesconfig;
+        private System.Windows.Forms.Label inputfile_tooltip;
+        private System.Windows.Forms.Label outputfile_tooltip;
+    }
+}
+
---
+++ WatsonTTS_UI/WatsonTTS_UI/Home.cs
@@ -0,0 +1,199 @@
+﻿using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Cloud.SDK.Core.Http;
+using IBM.Cloud.SDK.Core.Http.Exceptions;
+using IBM.Watson.TextToSpeech.v1;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WatsonTTS_UI
+{
+    public partial class Home : Form
+    {
+        public string PresetPath
+        {
+            get
+            {
+                return Directory.GetCurrentDirectory() + @"\watson_tts_ui_presets.config";
+            }
+        }
+
+        public Home()
+        {
+            InitializeComponent();
+            if (File.Exists(PresetPath))
+            {
+                string[] presets = File.ReadAllLines(PresetPath);
+                if (presets.Length == 4)
+                {
+                    try
+                    {
+                        authkey.Text = presets[0];
+                        watsonurl.Text = presets[1];
+                        inputfile.Text = presets[2];
+                        outputfile.Text = presets[3];
+                        OutToLog($"Presets were loaded.", true);
+                    }
+                    catch (Exception ex)
+                    {
+                        OutToLog(@"Error: Presets couldn't be loaded.");
+                        OutToLog($"Error description: {ex.Message}", true);
+                    }
+                }
+                else
+                {
+                    OutToLog(@"Error: Presets file was in an incompatible format.", true);
+                }
+            }
+        }
+
+        private void savepresets_Click(object sender, EventArgs e)
+        {
+            SavePresets();
+        }
+
+        private void SavePresets()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(PresetPath, false))
+                {
+                    sw.WriteLine(authkey.Text);
+                    sw.WriteLine(watsonurl.Text);
+                    sw.WriteLine(inputfile.Text);
+                    sw.WriteLine(outputfile.Text);
+                    sw.Close();
+                }
+
+                OutToLog($"Presets were saved to {PresetPath}.", true);
+            }
+            catch (Exception ex)
+            {
+                OutToLog(@"Error: Presets couldn't be created.");
+                OutToLog($"Error description: {ex.Message}", true);
+            }
+        }
+
+        private void OutToLog(string output, bool space = false)
+        {
+            string additionalSpace = "";
+            if (space)
+            {
+                additionalSpace = "\r\n";
+            }
+
+            if (!string.IsNullOrWhiteSpace(log.Text))
+            {
+                log.AppendText("\r\n" + output + additionalSpace);
+            }
+            else
+            {
+                log.AppendText(output + additionalSpace);
+            }
+            log.ScrollToCaret();
+        }
+
+        private void launchtts_Click(object sender, EventArgs e)
+        {
+            SavePresets();
+            LockUI(true);
+
+            // Content preparation
+            string inputPath = inputfile.Text;
+            if (!File.Exists(inputPath))
+            {
+                inputPath = Directory.GetCurrentDirectory() + @"\" + inputPath;
+
+                if (!File.Exists(inputPath))
+                {
+                    inputPath = Directory.GetCurrentDirectory() + inputPath;
+
+                    if (!File.Exists(inputPath))
+                    {
+                        OutToLog("Error: The input path is invalid.", true);
+                        LockUI(false);
+                        return;
+                    }
+                }
+            }
+            OutToLog($"The input path is: {inputPath}");
+            OutToLog($"The UI will freeze now until synthetization is completed.", true);
+
+            string content = "<speak version=\"1.0\"><prosody pitch=\"-1st\" rate=\"-10%\">" +
+                String.Join(Environment.NewLine, File.ReadAllLines(inputPath)) +
+                "</prosody></speak>";
+
+            // Sign in to Watson and synthesize
+            DateTime start = DateTime.Now;
+            DetailedResponse<MemoryStream> synthResult;
+            try
+            {
+                IamAuthenticator authenticator = new IamAuthenticator(
+                    apikey: authkey.Text
+                );
+                TextToSpeechService textToSpeech = new TextToSpeechService(authenticator);
+                textToSpeech.SetServiceUrl(watsonurl.Text);
+
+                synthResult = textToSpeech.Synthesize(
+                    text: content,
+                    accept: "audio/wav;rate=48000",
+                    voice: "en-US_MichaelV3Voice"
+                );
+            }
+            catch (ServiceResponseException ex)
+            {
+                OutToLog("Error occured during synthetization.", true);
+                OutToLog(ex.Message);
+                OutToLog(ex.Response.ToString());
+                OutToLog($"{ex.ResponseMessage.StatusCode}: {ex.ResponseMessage.RequestMessage.Content}");
+                LockUI(false);
+                return;
+            }
+            catch (Exception ex)
+            {
+                OutToLog(@"Error occured during synthetization.", true);
+                OutToLog($"Error description: {ex.Message}");
+                LockUI(false);
+                return;
+            }
+            DateTime end = DateTime.Now;
+            OutToLog($"Synthentization done in {(end - start).TotalSeconds} seconds.", true);
+
+            // Create audiofile
+            try
+            {
+                string outputFilePath = Directory.GetCurrentDirectory() + @"\" + (outputfile.Text.Length > 0 ? outputfile.Text + ".wav" : "audio.wav");
+                using (FileStream fs = File.Create(outputFilePath))
+                {
+                    synthResult.Result.WriteTo(fs);
+                    fs.Close();
+                    synthResult.Result.Close();
+                    OutToLog($"Synthetization was successful. The file is located at: {outputFilePath}");
+                    OutToLog("========================================", true);
+                    LockUI(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                OutToLog(@"Error: Output file creation failed.");
+                OutToLog($"Error description: {ex.Message}", true);
+                LockUI(false);
+                return;
+            }
+        }
+
+        private void LockUI(bool lockUI)
+        {
+            authkey.Enabled = !lockUI;
+            watsonurl.Enabled = !lockUI;
+            inputfile.Enabled = !lockUI;
+            outputfile.Enabled = !lockUI;
+        }
+
+        private void watsonconfig_label_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://cloud.ibm.com/services/text-to-speech/crn%3Av1%3Abluemix%3Apublic%3Atext-to-speech%3Aeu-de%3Aa%2Faaa049bd7fc048f993d071ec5e2dfd24%3Ac7be80fe-a6be-493c-83d6-fa1866e63c41%3A%3A?paneId=manage");
+        }
+    }
+}
